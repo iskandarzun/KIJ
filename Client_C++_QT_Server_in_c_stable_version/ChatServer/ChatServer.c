6395823bf006ca7ae88b6884fd45fa9993c34503
Iskandar <handyboy.ikhank@gmail.com>
2015-04-15 12:20:49 +0700

    Enkripsi server (masih error, baru bisa one way)

--- Client_C++_QT_Server_in_c_stable_version/ChatServer/ChatServer.c
+++ Client_C++_QT_Server_in_c_stable_version/ChatServer/ChatServer.c
@@ -42,10 +42,622 @@ client_data *head = NULL; //pointer linked list
 char users_online[20][20]; //belum diimplementasi
 int user_count = 0; //belum diimplementasi
 
+/******************************
+****** Enkripsi AES + OFB *****
+*******************************/
+#define Nb 4
+#define MAX_DIVIDE MAX_BUFFER / 16
+
+// Round dalam AES, Inisialisasi = 0
+int Nr=0;
+
+// satuan bit Key, Inisialisasi = 0
+int Nk=0;
+
+int getSBoxValue(int num)
+{
+    int sbox[256] =   {
+        //0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
+        0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76, //0
+        0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0, //1
+        0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15, //2
+        0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75, //3
+        0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84, //4
+        0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf, //5
+        0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8, //6
+        0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2, //7
+        0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73, //8
+        0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb, //9
+        0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79, //A
+        0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08, //B
+        0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a, //C
+        0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e, //D
+        0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf, //E
+        0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 }; //F
+    return sbox[num];
+}
+
+// The round constant word array, Rcon[i], contains the values given by
+// x to th e power (i-1) being powers of x (x is denoted as {02}) in the field GF(28)
+// Note that i starts at 1, not 0).
+int Rcon[255] = {
+    0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a,
+    0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39,
+    0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a,
+    0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8,
+    0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef,
+    0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc,
+    0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b,
+    0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3,
+    0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94,
+    0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20,
+    0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35,
+    0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f,
+    0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04,
+    0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63,
+    0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd,
+    0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb  };
+
+// Ekspansi key
+void KeyExpansion(unsigned char RoundKey[240], unsigned char Key[32])
+{
+    int i,j;
+    unsigned char temp[4],k;
+    
+    // round key pertama.
+    for(i=0;i<Nk;i++)
+    {
+        RoundKey[i*4]=Key[i*4];
+        RoundKey[i*4+1]=Key[i*4+1];
+        RoundKey[i*4+2]=Key[i*4+2];
+        RoundKey[i*4+3]=Key[i*4+3];
+    }
+    
+    // round key untuk round lainnya.
+    while (i < (Nb * (Nr+1)))
+    {
+        for(j=0;j<4;j++)
+        {
+            temp[j]=RoundKey[(i-1) * 4 + j];
+        }
+        if (i % Nk == 0)
+        {
+            // Fungsi rotasi 4 byte ke kiri.
+            // [a0,a1,a2,a3] jadi [a1,a2,a3,a0]
+            
+            // Fungsi RotWord()
+            {
+                k = temp[0];
+                temp[0] = temp[1];
+                temp[1] = temp[2];
+                temp[2] = temp[3];
+                temp[3] = k;
+            }
+            
+            // SubWord() fungsi yang mengambil 4 byte word
+            // dan dilakukan subtitusi terhadap S-Box.
+            
+            // Fungsi Subword()
+            {
+                temp[0]=getSBoxValue(temp[0]);
+                temp[1]=getSBoxValue(temp[1]);
+                temp[2]=getSBoxValue(temp[2]);
+                temp[3]=getSBoxValue(temp[3]);
+            }
+            
+            temp[0] =  temp[0] ^ Rcon[i/Nk];
+        }
+        else if (Nk > 6 && i % Nk == 4)
+        {
+            // Function Subword()
+            {
+                temp[0]=getSBoxValue(temp[0]);
+                temp[1]=getSBoxValue(temp[1]);
+                temp[2]=getSBoxValue(temp[2]);
+                temp[3]=getSBoxValue(temp[3]);
+            }
+        }
+        RoundKey[i*4+0] = RoundKey[(i-Nk)*4+0] ^ temp[0];
+        RoundKey[i*4+1] = RoundKey[(i-Nk)*4+1] ^ temp[1];
+        RoundKey[i*4+2] = RoundKey[(i-Nk)*4+2] ^ temp[2];
+        RoundKey[i*4+3] = RoundKey[(i-Nk)*4+3] ^ temp[3];
+        i++;
+    }
+}
+
+// Fungsi menambahkan round key terhadap state.
+// Round key ditambahkan dengan XOR function.
+void AddRoundKey(unsigned char RoundKey[240], int round, unsigned char state[4][4])
+{
+    int i,j;
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            state[j][i] ^= RoundKey[round * Nb * 4 + i * Nb + j];
+        }
+    }
+}
+
+// SubBytes adalah fungsi subtitusi nilai
+// array state dengan nilai di S-box.
+void SubBytes(unsigned char state[4][4])
+{
+    int i,j;
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            state[i][j] = getSBoxValue(state[i][j]);
+            
+        }
+    }
+}
+
+// Fungsi ShiftRows() adalah fungsi menggeser array ke kiri.
+// Setiap row digeser dengan perbedaan offset.
+// Offset = nomor Row. Jadi row awal(row ke 0) tidak digeser.
+void ShiftRows(unsigned char state[4][4])
+{
+    unsigned char temp;
+    
+    // Rotasi row pertama 1 kolom ke kiri
+    temp=state[1][0];
+    state[1][0]=state[1][1];
+    state[1][1]=state[1][2];
+    state[1][2]=state[1][3];
+    state[1][3]=temp;
+    
+    // Rotasi row kedua 2 kolom ke kiri
+    temp=state[2][0];
+    state[2][0]=state[2][2];
+    state[2][2]=temp;
+    
+    temp=state[2][1];
+    state[2][1]=state[2][3];
+    state[2][3]=temp;
+    
+    // Rotasi row ketiga 3 kolom ke kiri
+    temp=state[3][0];
+    state[3][0]=state[3][3];
+    state[3][3]=state[3][2];
+    state[3][2]=state[3][1];
+    state[3][1]=temp;
+}
+
+// xtime adalah variabel macro untuk menemukan product dari {02} dan argument xtime modulo {1b}
+#define xtime(x)   ((x<<1) ^ (((x>>7) & 1) * 0x1b))
+
+// MixColumns function mixes the columns of the state matrix
+void MixColumns(unsigned char state[4][4])
+{
+    int i;
+    unsigned char Tmp,Tm,t;
+    for(i=0;i<4;i++)
+    {
+        t=state[0][i];
+        Tmp = state[0][i] ^ state[1][i] ^ state[2][i] ^ state[3][i] ;
+        Tm = state[0][i] ^ state[1][i] ; Tm = xtime(Tm); state[0][i] ^= Tm ^ Tmp ;
+        Tm = state[1][i] ^ state[2][i] ; Tm = xtime(Tm); state[1][i] ^= Tm ^ Tmp ;
+        Tm = state[2][i] ^ state[3][i] ; Tm = xtime(Tm); state[2][i] ^= Tm ^ Tmp ;
+        Tm = state[3][i] ^ t ; Tm = xtime(Tm); state[3][i] ^= Tm ^ Tmp ;
+    }
+}
+
+// Cipher adalah main function yang mengenkripsi PlainText.
+void Cipher(unsigned char RoundKey[240], unsigned char in[16], unsigned char out[16], unsigned char state[4][4])
+{
+    int i,j,round=0;
+    
+    //Copy input PlainText ke state array.
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            state[j][i] = in[i*4 + j];
+        }
+    }
+    
+    // Tambahkan round key pertama ke state sebelum memulai rounds.
+    AddRoundKey(RoundKey, 0, state);
+    
+    // Akan ada rounds sebanyak Nr.
+    // Nr-1 rounds pertama identik.
+    // Nr-1 rounds dijalankan dengan loop dibawah.
+    for(round=1;round<Nr;round++)
+    {
+        SubBytes(state);
+        ShiftRows(state);
+        MixColumns(state);
+        AddRoundKey(RoundKey, round, state);
+    }
+    
+    // Round terakhir.
+    // Fungsi MixColumns tidak dilakukan di round ini.
+    SubBytes(state);
+    ShiftRows(state);
+    AddRoundKey(RoundKey, Nr, state);
+    
+    // Proses enkripsi berakhir.
+    // Copy state array ke output array.
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            out[i*4+j]=state[j][i];
+        }
+    }
+}
+
+
+/******************************
+ ********* End of AES *********
+ ******************************/
+
+// Fungsi ubah hex->digit
+char hexDigit(unsigned n)
+{
+    if (n < 10) {
+        return n + '0';
+    } else {
+        return (n - 10) + 'A';
+    }
+}
+
+// Fungsi ubah char->hex
+void charToHex(char c, char hex[2])
+{
+    hex[0] = hexDigit(c / 0x10);
+    hex[1] = hexDigit(c % 0x10);
+    //hex[2] = '\0';
+}
+
+// Fungsi ubah hex->Ascii
+char hexToAscii(char first, char second)
+{
+    char hex[5], *stop;
+    hex[0] = '0';
+    hex[1] = 'x';
+    hex[2] = first;
+    hex[3] = second;
+    hex[4] = 0;
+    return strtol(hex, &stop, 16);
+}
+
+
+// Fungsi ubah unsigned char -> char
+void uncharToChar(char *dst, unsigned char *src,size_t src_len)
+{
+    while (src_len--)
+        dst += sprintf(dst,"%02x",*src++);
+    *dst = '\0';
+}
+
+// Fungsi ubah string hex -> ascii
+void convertToReal(char *dest, char *source)
+{
+    int i;
+    int count = 0;
+    for(i = 0; i < strlen(source)/2; i++)
+    {
+        if(source[count] == '0' && source[count+1] == '0')
+        {
+            dest[i] = 0;
+        }
+        else
+        {
+            dest[i] = hexToAscii(source[count], source[count+1]);
+        }
+        
+        count += 2;
+    }
+}
+
+// Fungsi menyimpan hex value per array
+void containHex(char dest[MAX_BUFFER][1], char *source)
+{
+    int i;
+    int count = 0;
+    for(i = 0; i < strlen(source)/2; i++)
+    {
+        dest[i][0] = hexToAscii(source[count], source[count+1]);
+        count += 2;
+    }
+}
+
+// Fungsi char -> hex versi 2
+void char2hex(char* A, char *Hex) {
+    int i=0;
+    int j=0;
+    int t;
+    while(A[i]!=0) {
+        t = A[i] >> 4;
+        if (t < 10) {
+            Hex[j] = '0' + t;
+        }
+        else {
+            Hex[j] = 'a' + t - 10;
+        }
+        j++;
+        t = A[i] % 16;
+        if (t < 10) {
+            Hex[j] = '0' + t;
+        }
+        else {
+            Hex[j] = 'a' + t - 10;
+        }
+        j++;
+        i++;
+    }
+}
+
+// Fungsi xor versi 2
+void xor_str(char *A, char *B, char *res) {
+    int i = 0;
+    int a, b, x;
+    
+    while (A[i] != 0) {//kalau belum null
+        if (A[i] >= '0' && A[i] <= '9') {
+            a = A[i] - '0';
+        }
+        else if (A[i] >= 'a' && A[i] <= 'z'){
+            a = A[i] - 'a' + 10;
+        }
+        if (B[i] >= '0' && B[i] <= '9') {
+            b = B[i] - '0';
+        }
+        else if (B[i] >= 'a' && B[i] <= 'z'){
+            b = B[i] - 'a' + 10;
+        }
+        x = a ^ b;
+        if (x < 10) {
+            res[i] = '0' + x;
+        }
+        else {
+            res[i] = 'a' + x - 10;
+        }
+        i++;
+    }
+    res[i] = 0;
+}
+
+// Fungsi XOR
+char *xor(char *source1, int src_sz, char *source2) {
+    
+    int i, j = 0;
+    int k_len = strlen(source2);
+    char *result = calloc(src_sz+1, sizeof(char));
+    
+    for (i = 0; i < src_sz; i++) {
+        result[i] = source1[i] ^ source2[j];
+        j++;
+        j = j > k_len ? 0 : j;
+        //printf("perulangan -> %d\nlength -> %d\nkarakter -> %c\n", i, src_sz, result[i]);
+    }
+    return result;
+}
+
+// Fungsi ubah string char -> hex
+char* stringToHex(char* input, int size)
+{
+    static const char* const lut = "0123456789ABCDEF";
+    size_t len = size;
+    
+    char output[100];
+    int count = 0;
+    
+    for (size_t i = 0; i < len; ++i)
+    {
+        const unsigned char c = input[i];
+        output[count] = lut[c >> 4];
+        count++;
+        output[count] = lut[c & 15];
+        count++;
+    }
+    return output;
+}
+
+// Fungsi memisah menjadi 16 byte input
+void divideAscii(char dest[MAX_BUFFER][17], char *source)
+{
+    int i, j, part;
+    part = (strlen(source) - 1) / 16;
+    //printf("part : %d\nlength : %d\n", part, strlen(source));
+    for(i = 0; i <= part; i++)
+    {
+        memset(&dest[i][0], 0, sizeof(dest[i]));
+        for(j = i * 16; j < ((i+1) * 16); j++)
+        {
+            if(j > strlen(source))
+            {
+                dest[i][16] = '\0';
+                break;
+            }
+            dest[i][j%16] = source[j];
+            //printf("karakter : %c\n", source[j]);
+            //printf("%d -> %d\n", j, strlen(dest[i]));
+            
+        }
+    }
+    //printf("%d\n", strlen(dest[0]));
+}
+
 //fungsi write data (encrypt here)
 void send_data(int socket, char output[MAX_BUFFER])
 {
-    write(socket,output,strlen(output));
+    //Enkripsi
+    
+    // in - array dari pesan yang dienkripsi.
+    // out - array hasil enkripsi.
+    // state - array hasil sementara enkripsi.
+    unsigned char in[16], out[16], state[4][4];
+    
+    // Round key.
+    unsigned char RoundKey[240];
+    
+    // key input
+    unsigned char Key[32];
+    
+    char convert[MAX_BUFFER] = "";
+    char deconvert[MAX_BUFFER] = "";
+    char real[MAX_BUFFER] = "";
+    int b, i, blocksIV, blocksAscii;
+    
+    //AES mode (128, 192, 256) :
+    int AES_Mode = 128;
+    
+    //assign Nk dan Nr
+    Nk = AES_Mode / 32;
+    Nr = Nk + 6;
+    
+    //Initialization Vector
+    char IV[MAX_BUFFER] = "0123456789abcdef";
+    
+    //Key Enkripsi
+    unsigned char tempkey[32] = {0x00  ,0x01  ,0x02  ,0x03  ,0x04  ,0x05  ,0x06  ,0x07  ,0x08  ,0x09  ,0x0a  ,0x0b  ,0x0c  ,0x0d  ,0x0e  ,0x0f};
+    
+    for(i=0;i<Nk*4;i++)
+    {
+        Key[i]=tempkey[i];
+    }
+    
+    blocksAscii = ((strlen(output) - 1) / 16);
+    
+    //dipisah 16 byte
+    char partAscii[MAX_BUFFER][17];
+    divideAscii(partAscii, output);
+    
+    char hasilxorencrypt[MAX_DIVIDE][33];
+    char hasilxordecrypt[MAX_DIVIDE][33];
+    
+    //Mulai enkripsi
+    for (int ba = 0; ba <= blocksAscii; ba++)
+    {
+        if(ba == 0)
+        {
+            blocksIV = ((strlen(IV) - 1) / 16);
+            //printf("jumlah : %d\n", blocksIV);
+        }
+        else
+        {
+            blocksIV = ((strlen(convert) - 1) / 16);
+        }
+        
+        for(b = 0; b <= blocksIV; b++)
+        {
+            for(i=0;i<Nb*4;i++)
+            {
+                if(ba == 0)
+                {
+                    if((i + (b * 16) < strlen(IV)))
+                    {
+                        in[i] = (unsigned char)IV[ (i + (b * 16) ) ];
+                    }
+                    else
+                    {
+                        in[i] = 0x00;
+                    }
+                }
+                else
+                {
+                    if((i + (b * 16) < strlen(convert)))
+                    {
+                        in[i] = (unsigned char)convert[ (i + (b * 16) ) ];
+                    }
+                    else
+                    {
+                        in[i] = 0x00;
+                    }
+                }
+            }
+            
+            printf("\n");
+            
+            printf("hexadecimal in in: ");
+            
+            for(i=0;i<Nb*4;i++)
+            {
+                printf("%02x ", in[i]);
+            }
+            printf("\n");
+            
+            // Fungsi KeyExpansion untuk ekspan key
+            KeyExpansion(RoundKey, Key);
+            
+            // Fungsi Cipher
+            Cipher(RoundKey, in, out, state);
+            
+            // Output encrypted text.
+            printf("\nText after encryption:\n");
+            
+            printf("Encryption : ");
+            
+            for(i=0;i<Nb*4;i++)
+            {
+                printf("%02x ", out[i]);
+            }
+            printf("\n\n");
+            
+            char tempconvert[16];
+            uncharToChar(tempconvert, out, sizeof(out));
+            
+            memset(&convert[0], 0, sizeof(convert));
+            strcat(convert, tempconvert);
+            
+            memset(&in[0], 0, sizeof(in));
+            memset(&out[0], 0, sizeof(out));
+        }
+        
+        
+        char plainInHex[MAX_BUFFER];
+        //char *templagi;
+        char xorResult[MAX_BUFFER];
+        
+        /*
+         for(i = 0; i < strlen(partAscii[ba]); i++)
+         {
+         plainInHex[i] = (unsigned char) partAscii[ba][i];
+         }
+         
+         uncharToChar(templagi, plainInHex, sizeof(plainInHex));
+         strcat(convResult, templagi);
+         */
+        
+        /*
+        char2hex(partAscii[ba], plainInHex);
+        xor_str(plainInHex, convert, xorResult);
+        */
+        //strcpy(convResult, stringToHex(partAscii[ba], strlen(partAscii[ba])));
+        
+        //strcpy(hasilxorencrypt[ba], xorResult);
+        strcpy(hasilxorencrypt[ba], xor(partAscii[ba], strlen(partAscii[ba]), convert));
+        //printf("hasil konversi ke hex partAscii -> %s\n", convResult);
+        /*
+        printf("panjang partAscii -> %d\nkali 2 -> %d\n", strlen(partAscii[ba]), strlen(partAscii[ba])*2);
+        printf("panjang hasilxorencrypt -> %d\n", strlen(hasilxorencrypt[ba]));
+        printf("panjang convert -> %d\n", strlen(convert));
+        printf("hasil convert blok ke %d : ", ba);
+        puts(convert);
+        printf("hasil xor encryption : ");
+        puts(hasilxorencrypt[ba]);
+         */
+        
+        char tempConvResult[MAX_BUFFER];
+        convertToReal(tempConvResult, convert);
+        memset(&convert[0], 0, sizeof(convert));
+        strcpy(convert, tempConvResult);
+        //memset(&plainInHex[0], 0, sizeof(plainInHex));
+        //memset(&xorResult[0], 0, sizeof(xorResult));
+    }
+    
+    char encrypted_message[MAX_BUFFER] = "";
+    for(i = 0; i < blocksAscii; i++)
+    {
+        strcat(encrypted_message, hasilxorencrypt[i]);
+    }
+    //Selesai
+    printf("message -> %s\n", encrypted_message);
+    write(socket,encrypted_message,strlen(encrypted_message));
 }
 
 //fungsi read data (decrypt here)
