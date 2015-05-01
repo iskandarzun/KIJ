e66954e6375d8c0b9a3a64454ada27d9e437bb52
Iskandar <handyboy.ikhank@gmail.com>
2015-05-01 20:34:13 +0700

    fixed structure express

--- Tugas_2_DigitalCertificate/app.js
+++ Tugas_2_DigitalCertificate/app.js
@@ -42,9 +42,11 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
+    if(err.status == undefined) err.status = 'Implementation Error';
     res.render('error', {
       message: err.message,
-      error: err
+      error: err,
+      status: err.status
     });
   });
 }
@@ -55,7 +57,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
+    status: err.status
   });
 });
 app.listen(port);
