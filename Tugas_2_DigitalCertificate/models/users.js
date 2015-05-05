426240fcd648cb97c617779fbb6fb48407e31b5d
vertikaldash <vij.fathurahman@gmail.com>
2015-05-05 23:17:12 +0700

    models

--- Tugas_2_DigitalCertificate/models/users.js
+++ Tugas_2_DigitalCertificate/models/users.js
@@ -1,1 +1,24 @@
-disini yayang vijay
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var usersSchema = new Schema({
+	orgName : String,
+	address : String,
+	city : String,
+	state : String,
+	zipCode : String,
+	country : String,
+
+	jobTitle : String,
+	firstName : String,
+	lastName : String,
+	contactEmail : String,
+	contactTelp : String,
+
+	username : String,
+	password : String,
+
+	companyType : String
+});
+
+mongoose.model('users', usersSchema);
